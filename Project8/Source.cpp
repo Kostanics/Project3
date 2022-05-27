ebf1ded538d370d9d4f0dd8a42525d65fd173255
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-05-27 04:14:25 +0300

    Update Source.cpp

--- Project8/Source.cpp
+++ Project8/Source.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <chrono> 
 using namespace std;
-void Merge(int* A, int first, int last){
-    int start, middle,  final, j;
-    int* mas = new int[last+1];
-    middle = (first + last) / 2; 
-    start = first; 
-    final = middle + 1; 
+int swapcount = 0, ifcount = 0;
+void Merge(int* A, int first, int last) {
+    int start, middle, final, j;
+    int* mas = new int[last + 1];
+    middle = (first + last) / 2;
+    start = first;
+    final = middle + 1;
     for (j = first; j <= last; j++) {
         if ((start <= middle) && ((final > last) || (A[start] < A[final]))) {
             mas[j] = A[start];
@@ -22,29 +24,32 @@ void Merge(int* A, int first, int last){
     delete[]mas;
 }
 
-void MergeSort(int* A, int first, int last){
-    if (first < last){
-        MergeSort(A, first, (first + last) / 2); 
-        MergeSort(A, (first + last) / 2 + 1, last); 
-        Merge(A, first, last); 
+void MergeSort(int* A, int first, int last) {
+    if (first < last) {
+        MergeSort(A, first, (first + last) / 2);
+        MergeSort(A, (first + last) / 2 + 1, last);
+        Merge(A, first, last);
     }
 }
 
-void sorty(const int n){
-    int* A = new int[n+1];
-    cout << "not sort:"<<endl;
+void sorty(const int n) {
+    int* A = new int[n + 1];
+    //cout << "not sort:" << endl;
     for (int i = 1; i <= n; i++)
     {
         A[i] = rand() % 10;
-        cout << A[i];
+        //cout << A[i];
     }
+    auto start = chrono::high_resolution_clock::now();
     MergeSort(A, 1, n);
-    cout<< endl << "sort:"<<endl;
+    auto end = chrono::high_resolution_clock::now();
+    cout << "sorting time: " << ((chrono::duration_cast<chrono::microseconds>)(end - start)).count() << " (microseconds)" << endl;
+    //cout << endl << "sort:" << endl;
     for (int i = 1; i <= n; i++) {
-        cout << A[i];
+        //cout << A[i];
     }
     delete[]A;
-    }
+}
 
 int main()
 {
